f2b9d822ee84588b4303fb999daa3c0deb3528a4
Peter Tlučhoř <tluchor.peter@outlook.cz>
2024-08-03 18:50:29 +0200

    začátek projektu

---
+++ projekt_1.py
@@ -0,0 +1,19 @@
+"""
+
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Peter Tlučhoř
+
+email: tluchor.peter@outlook.com
+
+discord: Tlucek#0754
+
+"""
+
+# Nejdřív musím vytvořit databázi registrovaných uživatelů a jejich hesel. Jelikož se jedná o páry klíč+hodnota, vytvořím dict.
+registered = dict(
+    "bob": 123
+    "ann": "pass123"
+    "mike": "password123"
+    "liz": "pass123"
+)
