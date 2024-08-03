18013d5b7a43d21ce1080a7f1fb0cdbc72135483
Peter Tlučhoř <tluchor.peter@outlook.cz>
2024-08-03 19:21:28 +0200

    vytvořen input s ověřením

--- projekt_1.py
+++ projekt_1.py
@@ -11,9 +11,22 @@ discord: Tlucek#0754
 """
 
 # Nejdřív musím vytvořit databázi registrovaných uživatelů a jejich hesel. Jelikož se jedná o páry klíč+hodnota, vytvořím dict.
-registered = dict(
-    "bob": 123
-    "ann": "pass123"
-    "mike": "password123"
+registered = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
     "liz": "pass123"
-)
+}
+username = input("Zadejte přihlašovací jméno") # vloží username a musím ověřit, zda se nachází v dictu. Když ne, vypíšu hlášku a vyzvu ho znovu.
+password = input("Zadejte heslo")
+
+def verify_user(username, password):
+    if username in registered and registered[username] == password:
+        return True
+    else:
+        return False
+
+if verify_user(username, password):
+    print("ok")
+else:
+    print("neok")
