a86d230fbdde0bbe32aee52b141a88047084ae84
Peter Tlučhoř <tluchor.peter@outlook.cz>
2024-08-09 20:27:31 +0200

    úprava loop a změna if na while

--- projekt_1.py
+++ projekt_1.py
@@ -31,35 +31,31 @@ def verify_user(username, password):
 
 if verify_user(username, password):
     print(f"Vítej v textovém analyzátoru, {username}!")
-    from texty import TEXTS # importuji texty z externího souboru
-    text_1 = TEXTS[0]
-    text_2 = TEXTS[1]
-    text_3 = TEXTS[2]
-    print("=" * 50)
-    print("VYBER SI TEXT K ANALÝZE")
-    print("=" * 50)
-    print("Text 1:", sep="\n")
-    print(text_1)
-    print("-" * 50)
-    print("Text 2:", sep="\n")
-    print(text_2)
-    print("-" * 50)
-    print("Text 3:", sep="\n")
-    print(text_3)
-    print("=" * 50)
 else:
     print("Špatný login. Zkuste to znova. Program se nyní ukončí")
 
-vstup = int(input("Zadejte číslo textu k analýze (1 - 3)"))
-if vstup == 1:
-    vybrany_text = TEXTS[0]
-elif vstup == 2:
-    vybrany_text = TEXTS[1]
-elif vstup == 3:
-    vybrany_text = TEXTS[3]
+from texty import TEXTS # importuji texty z externího souboru
+text_1 = TEXTS[0]
+text_2 = TEXTS[1]
+text_3 = TEXTS[2]
+print("=" * 55)
+print("VYBER SI TEXT K ANALÝZE")
+print("=" * 55)
+
+vybrany_text = None
+while vybrany_text is None:
+    vstup = int(input("Zadejte číslo textu k analýze (1 - 3): "))
+    if vstup == 1:
+        vybrany_text = TEXTS[0]
+        break
+    elif vstup == 2:
+        vybrany_text = TEXTS[1]
+        break
+    elif vstup == 3:
+        vybrany_text = TEXTS[2]
+        break
 else:
-    vybrany_text = None
     print("Neplatná volba. Zadejte číslo 1, 2 nebo 3.")
-if vybrany_text:
-    print("Zvolili jste následující text:")
-    print(vybrany_text)
+print("Zvolili jste následující text:", sep="\n")
+print("-" * 55)
+print(vybrany_text)
