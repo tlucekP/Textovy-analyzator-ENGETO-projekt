157229b5bacb3a25c43102a15fbf8ac6b09bbb28
Peter Tlučhoř <tluchor.peter@outlook.cz>
2024-08-09 20:08:21 +0200

    nahrání TEXTS a výzva k výběru

--- projekt_1.py
+++ projekt_1.py
@@ -17,10 +17,13 @@ registered = {
     "mike": "password123",
     "liz": "pass123"
 }
-username = input("Zadejte přihlašovací jméno: ") # vloží username a musím ověřit, zda se nachází v dictu. Když ne, vypíšu hlášku a vyzvu ho znovu.
+username = input("Zadejte přihlašovací jméno: ") # vloží username heslo a musím ověřit, zda se nachází v dictu. Když ne, vypíšu hlášku a vyzvu ho znovu a program ukončím.
 password = input("Zadejte heslo: ")
 
 def verify_user(username, password):
+    """
+    Ověří, zda zadané vstupy username a password jsou registrované a zda vzájemně tvoří login do aplikace
+    """
     if username in registered and registered[username] == password:
         return True
     else:
@@ -28,5 +31,35 @@ def verify_user(username, password):
 
 if verify_user(username, password):
     print(f"Vítej v textovém analyzátoru, {username}!")
+    from texty import TEXTS # importuji texty z externího souboru
+    text_1 = TEXTS[0]
+    text_2 = TEXTS[1]
+    text_3 = TEXTS[2]
+    print("=" * 50)
+    print("VYBER SI TEXT K ANALÝZE")
+    print("=" * 50)
+    print("Text 1:", sep="\n")
+    print(text_1)
+    print("-" * 50)
+    print("Text 2:", sep="\n")
+    print(text_2)
+    print("-" * 50)
+    print("Text 3:", sep="\n")
+    print(text_3)
+    print("=" * 50)
 else:
     print("Špatný login. Zkuste to znova. Program se nyní ukončí")
+
+vstup = int(input("Zadejte číslo textu k analýze (1 - 3)"))
+if vstup == 1:
+    vybrany_text = TEXTS[0]
+elif vstup == 2:
+    vybrany_text = TEXTS[1]
+elif vstup == 3:
+    vybrany_text = TEXTS[3]
+else:
+    vybrany_text = None
+    print("Neplatná volba. Zadejte číslo 1, 2 nebo 3.")
+if vybrany_text:
+    print("Zvolili jste následující text:")
+    print(vybrany_text)
