da765b1f1330162d54aeb6250a81ddf1732dbe6f
tlucekP <160940948+tlucekP@users.noreply.github.com>
2024-08-18 23:00:48 +0200

    Add files via upload
    úprava kodu pro přesnější analýzu slov a písmen

--- projekt_1.py
+++ projekt_1.py
@@ -11,86 +11,97 @@ discord: Tlucek#0754
 
 # Nejdřív musím vytvořit databázi registrovaných uživatelů a jejich hesel. Jelikož se jedná o páry klíč+hodnota, vytvořím dict. Musím importovat modul "sys"
 # abych využil "sys.exit" pro ukončení programu.
-import sys
+
+from texty import TEXTS # importuji texty z externího souboru
+import string # v analýze použiju modul string s proměnnou punctuation pro identifikaci interpunkčních znamének k očištění ve slovech
+
 registered = {
     "bob": "123",
     "ann": "pass123",
     "mike": "password123",
     "liz": "pass123"
 }
-username = input("Zadejte přihlašovací jméno: ") # vloží username heslo a musím ověřit, zda se nachází v dictu. Když ne, vypíšu hlášku a vyzvu ho znovu a program ukončím.
-password = input("Zadejte heslo: ")
+
+def main(): # celou analýzu textu zadefinuji do main funkce
+    vybrany_text = input("Zadejte číslo textu k analýze (1 - {}): ".format(len(TEXTS))) # využiji dynamický rozsah vstupu, kdyby v budoucnu přibyl další text
+    print("=" * 55)
+    if vybrany_text.isdigit(): # podmíním uživatelský vstup vložením čísla
+        vybrany_text = int(vybrany_text)
+    else:
+        print("Vložte číslo! Program se nyní ukončí. Zkuste to, prosím, znovu.")
+        return
+    
+    if vybrany_text-1 < 0 or vybrany_text > len(TEXTS):
+        print(f"Text číslo {vybrany_text} neexistuje. Zkuste to, prosím, znovu.")
+        return
+    
+    # očistím slova o nežádoucí interpunkční znaky v jedné proměnné, aby nemusel pořád procházet slova v jednotlivých podmínkách
+    translator = str.maketrans("", "", string.punctuation)
+    slova = TEXTS[vybrany_text-1].translate(translator).split()
+
+    counter = { # předpřipravený dict pro statistiku
+        "vsechnaslova": 0,
+        "Slova": 0,
+        "SLOVA": 0,
+        "slova": 0,
+        "cisla": 0,
+        "sum": 0,
+        "lenstat": {}
+    }
+
+    for slovo in slova: # iteruji text pro statistiku. Čísla definuji jako float, kdyby se tam float vyskytovalo. Využívám try-except.
+        slovo_delka = len(slovo)
+        try:
+            cislo = float(slovo)
+            counter["cisla"] += 1
+            counter["sum"] += cislo
+            # counter["vsechnaslova"] +=1
+        except ValueError:
+            pass
+        counter["vsechnaslova"] +=1
+        if slovo_delka in counter["lenstat"]:
+            counter["lenstat"][slovo_delka] += 1
+        else:
+            counter["lenstat"][slovo_delka] = 1
+        if slovo.isupper():
+            counter["SLOVA"] += 1
+        if slovo.islower():
+            counter["slova"] += 1
+        if slovo[0].isupper():
+            counter["Slova"] += 1
+                    
+    print(f"Ve vybraném textu je {counter['vsechnaslova']} slov.")
+    print(f"Ve vybraném textu je {counter['Slova']} začínajících velkým písmenem.")
+    print(f"Ve vybraném textu je {counter['SLOVA']} slov obsahující všechna písmena velká.")
+    print(f"Ve vybraném textu je {counter['slova']} slov obsahující všechna písmena malá.")
+    print(f"Ve vybraném textu je {counter['cisla']} počet čísel.")
+    print(f"Ve vybraném textu je {counter['sum']} součtem všech čísel")
+
+    max_frekvence = max(counter["lenstat"].values(), default=0) # max počet hvězdiček pro sloupec OCCURENCES
+    max_occurences = max(15, max_frekvence) # šířka sloupce OCCURENCES, musím zvolit dynamické zadání, protože nevím jak velká slova tam jsou
+
+    print(f"\nLEN| {'OCCURENCES'.center(max_occurences)} |NR") # jelikož tam můžou být různě dlouhá slova, musím použít formátování dle max_occurences
+
+    for delka, frekvence in sorted(counter["lenstat"].items()):
+        print(f"{delka:<3}| {'*' * frekvence:<{max_occurences}} |{frekvence:<2}") # využívám dynamické formátování textu při výpisu
 
 def verify_user(username, password):
     """
     Ověří, zda zadané vstupy username a password jsou registrované a zda vzájemně tvoří login do aplikace
     """
-    if username in registered and registered[username] == password:
-        return True
-    else:
-        return False
-
-if verify_user(username, password):
-    print(f"Vítej v textovém analyzátoru, {username}!")
-else:
-    print("Špatný login. Zkuste to znova. Program se nyní ukončí")
-    sys.exit()
-
-from texty import TEXTS # importuji texty z externího souboru
-text_1 = TEXTS[0]
-text_2 = TEXTS[1]
-text_3 = TEXTS[2]
-print("=" * 55)
-print("VYBER SI TEXT K ANALÝZE")
-print("=" * 55)
+    return username in registered and registered[username] == password
 
 def kontrola_vstupu(value):
     """Kontroluje, zda vstupní hodnota je integer v rozmězí 1-3."""
-    return value.isdigit() and 1 <= int(value) <= 3
-vybrany_text = None
-while vybrany_text is None:
-    vstup = input("Zadejte číslo textu k analýze (1 - 3): ")
-    if kontrola_vstupu(vstup):
-        vstup = int(vstup)
-        if vstup == 1:
-            vybrany_text = TEXTS[0]
-        elif vstup == 2:
-            vybrany_text = TEXTS[1]
-        elif vstup == 3:
-            vybrany_text = TEXTS[2]
+    return value.isdigit() and 1 <= int(value) <= len(TEXTS)
+
+if __name__ == "__main__":
+    username = input("Zadejte přihlašovací jméno: ")
+    password = input("Zadejte heslo: ")
+
+    if verify_user(username, password):
+        print(f"Vítej v textovém analyzátoru, {username}!")
+        print("=" * 55)
+        main()
     else:
-        print("Neplatná volba. Program se nyní ukončí. Zkuste to, prosím, znovu.")
-        sys.exit()
-print("Zvolili jste následující text:", sep="\n")
-print("-" * 55)
-print(vybrany_text)
-
-slova = vybrany_text.split() #musím rozdělit slova v textu
-pocet_slov = len(slova) # zjistím si počet slov
-slova_velke_pismeno_tit = sum(1 for slovo in slova if slovo.istitle()) # počet slov začínající velkým písmenem
-slova_velka_pismena = sum(1 for slovo in slova if slovo.isupper()) # počet slov obsahující velká písmena
-slova_mala_pismena = sum(1 for slovo in slova if slovo.islower()) # počet slov obsahující malá písmena
-pocet_cisel = sum(1 for slovo in slova if slovo.isdigit()) # počet čísel v textu
-suma_cisel = sum(int(slovo) for slovo in slova if slovo.isdigit()) # součet všech čísel v textu
-
-print("-" * 55)
-print(f"Ve vybraném textu je {pocet_slov} slov.")
-print(f"Ve vybraném textu je {slova_velke_pismeno_tit} slov začínajících velkým písmenem.")
-print(f"Ve vybraném textu je {slova_velka_pismena} slov obsahující všechna písmena velká.")
-print(f"Ve vybraném textu je {slova_mala_pismena} slov obsahující všechna písmena malá.")
-print(f"Ve vybraném textu je {pocet_cisel} počet čísel.")
-print(f"Ve vybraném textu je součet všch čísel {suma_cisel}.")
-print("-" * 55)
-
-delka_slova = {} # vytvořím dict pro výpočet četnosti délek slov
-for slovo in slova:
-    delka = len(slovo)
-    if delka in delka_slova:
-        delka_slova[delka] += 1
-    else:
-        delka_slova[delka] = 1
-max_frekvence = max(delka_slova.values()) # max počet hvězdiček pro sloupec OCCURENCES
-max_occurences = max(15, max_frekvence) # šířka sloupce OCCURENCES, musím zvolit dynamické zadání, protože nevím jak velká slova tam jsou
-print(f"\nLEN| {'OCCURENCES'.center(max_occurences)} |NR") # jelikož tam můžou být různě dlouhá slova, musím použít formátování dle max_occurences
-for delka, frekvence in sorted(delka_slova.items()):
-    print(f"{delka:<3}| {'*' * frekvence:<{max_occurences}} |{frekvence:<2}") # využívám dynamické formátování textu při výpisu
+        print("Špatný login. Zkuste to znova. Program se nyní ukončí.")
